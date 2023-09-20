2604e516cd1036d600de3c8502a6ba46d6920538
Jose Uriel Saenz Cuellar <urieljose500@gmail.com>
2023-09-19 21:28:14 -0500

    changeses

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import mysql.connector
 import icons_rc
 from PyQt5.QtGui import QPixmap
 import sys
+from functools import partial
 from db_consulta import get_database_connection
 
 class ExitDialog(QDialog):
@@ -33,12 +34,21 @@ class ExitDialog(QDialog):
 class HomePage(QWidget):
     def __init__(self):
         super().__init__()
+        self.cot = []
+        self.info = []
+        con = get_database_connection()
+        cursor = con.cursor()
+        sql = "SELECT * FROM cotizaciones;"
+        cursor.execute(sql)
+        self.cot = cursor.fetchall()
+        print(self.cot)
+        con.close()
+        
         self.initUI()
 
     def initUI(self):
         self.layout = QVBoxLayout()
-
-        # Barra de búsqueda
+        
         self.search_layout = QHBoxLayout()
         self.search_input = QLineEdit()
         self.search_button = QPushButton("Search")
@@ -66,58 +76,76 @@ class HomePage(QWidget):
         self.cards_container = QWidget()
         scroll_area.setWidget(self.cards_container)
 
-        # Usar un diseño de cuadrícula para organizar las tarjetas en filas y columnas
         self.grid_layout = QGridLayout()
         self.cards_container.setLayout(self.grid_layout)
 
-        # Crear tarjetas de ejemplo
-        self.cards = []
-        cars = [("uriel", "icons/search.png"), ("saenz", "icons/locks.png")]
-        d = 0
-        for i in cars:
-            card = self.create_card(d, i[0], i[1])  # Cambia el nombre y la imagen según tus necesidades
-            d += 1
-            self.cards.append(card)
+        self.card_widgets = []
+
+        for index, data in enumerate(self.cot):
+            card_info = self.create_card(index, data[1], "icons/coin.png")
+            self.card_widgets.append(card_info)
+            
+            # Utiliza functools.partial para crear una función lambda con el valor correcto de index
+            card_info["widget"].clicked.connect(partial(self.redirect_to_page, data[0]))
+
 
         self.setLayout(self.layout)
-
-        # Conecta el evento de texto cambiado al método de filtrado
+        
         self.search_input.textChanged.connect(self.filter_cards)
 
     def create_card(self, index, name, image_path):
-        # Crear etiqueta para la imagen
         image_label = QLabel()
         pixmap = QPixmap(image_path)
-        pixmap = pixmap.scaledToWidth(100)  # Cambiar el ancho de la imagen
+        pixmap = pixmap.scaledToWidth(300)
         image_label.setPixmap(pixmap)
 
-        # Crear etiqueta para el nombre
         name_label = QLabel(name)
 
-        # Crear un diseño vertical para la tarjeta (imagen arriba, nombre abajo)
         card_layout = QVBoxLayout()
         card_layout.addWidget(image_label, alignment=Qt.AlignCenter)
         card_layout.addWidget(name_label, alignment=Qt.AlignCenter)
 
-        # Agregar la tarjeta al diseño de la cuadrícula
+        card_widget = QPushButton()
+        card_widget.setFixedSize(320, 300)
+        card_widget.setStyleSheet(
+            "QPushButton {"
+            "    background-color: white;"
+            "    border-radius: 10px;"
+            "    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);"
+            "}"
+        )
+        card_widget.setCursor(Qt.PointingHandCursor)
+        card_widget.setLayout(card_layout)
+
         row = index // 5
         col = index % 5
-        self.grid_layout.addLayout(card_layout, row, col)
+        self.grid_layout.addWidget(card_widget, row, col)
 
-        return name_label
+        return {"widget": card_widget, "name": name, "image_path": image_path}
 
     def filter_cards(self):
         search_text = self.search_input.text().strip().lower()
-        for card in self.cards:
-            card_text = card.text().lower()
-            card_widget = card.parentWidget()
+        for card_info in self.card_widgets:
+            card_widget = card_info["widget"]
+            card_text = card_info["name"].lower()
             if search_text in card_text:
                 card_widget.setVisible(True)
             else:
                 card_widget.setVisible(False)
         if not search_text:
-            for card in self.cards:
-                card.parentWidget().setVisible(True)
+            for card_info in self.card_widgets:
+                card_widget = card_info["widget"]
+                card_widget.setVisible(True)
+
+    def redirect_to_page(self, index):
+        print(f"Tarjeta {str(index)} fue clicada. Redirigir a la página correspondiente.")
+        con = get_database_connection()
+        cursor = con.cursor()
+        sql = f"SELECT * FROM cotizaciones WHERE idCot = {index};"
+        cursor.execute(sql)
+        self.cot = cursor.fetchall()
+        print(self.cot)
+
 
 
 
@@ -153,14 +181,12 @@ class MainSlide(QtWidgets.QWidget):
         desktop = QtWidgets.QApplication.desktop()
         self.screen_geometry = desktop.screenGeometry()
 
-        # Main window setup
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setGeometry(self.screen_geometry)
 
         layout = QtWidgets.QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
 
-        # Header setup
         header = QtWidgets.QWidget()
         header.setStyleSheet("background-color: #db5e5e;")
         header.setFixedSize(self.screen_geometry.width(), 100)
@@ -171,10 +197,13 @@ class MainSlide(QtWidgets.QWidget):
         company_label = QtWidgets.QLabel("     Ingeniería Integral y Servicios")
         company_label.setStyleSheet("color: rgb(231, 231, 231); font: 15pt \"Verdana\"; font-weight: 900;")
 
-        # Agregar las redirecciones (botones) en una caja separada
         redirect_layout = QtWidgets.QHBoxLayout()
         redirect_layout.setContentsMargins(0, 0, 0, 0)
         redirect = ["Inicio", "Cotizaciones", "Precios"]
+        
+        # Lista de botones de redirección
+        self.redirect_buttons = []
+
         for i in range(3):
             redirect_button = QtWidgets.QPushButton(redirect[i])
             redirect_button.setStyleSheet("""
@@ -184,14 +213,15 @@ class MainSlide(QtWidgets.QWidget):
                     padding: 6px;
                     color: white; 
                     font: 13pt "Verdana";
-                    
                 }
             """)
-            redirect_button.setCursor(Qt.PointingHandCursor)
-            redirect_button.clicked.connect(lambda _, i=i: self.change_page(i))  # Conectar el clic del botón con el cambio de página
+            redirect_button.setCursor(QtCore.Qt.PointingHandCursor)
+            redirect_button.clicked.connect(lambda _, i=i: self.change_page(i))
             redirect_layout.addWidget(redirect_button, alignment=QtCore.Qt.AlignRight)
 
-        # Botón de cierre (X) en la esquina superior derecha
+            # Agregar el botón a la lista
+            self.redirect_buttons.append(redirect_button)
+
         close_button = QtWidgets.QPushButton("X")
         close_button.setStyleSheet("""
             QPushButton {
@@ -214,7 +244,6 @@ class MainSlide(QtWidgets.QWidget):
         close_button.setMaximumSize(QtCore.QSize(35, 25))
         close_button.clicked.connect(self.show_exit_dialog)
 
-        # Agregar elementos al encabezado
         header_layout.addWidget(company_label, alignment=QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
         header_layout.addStretch(1)
         header_layout.addLayout(redirect_layout)
@@ -222,30 +251,53 @@ class MainSlide(QtWidgets.QWidget):
 
         layout.addWidget(header)
 
-        # Crear un contenedor para las páginas (bodies)
         self.page_container = QStackedWidget()
-        self.page_container.addWidget(HomePage())  # Agrega la página de inicio
-        self.page_container.addWidget(CotizacionesPage())  # Agrega la página de cotizaciones
-        self.page_container.addWidget(PreciosPage())  # Agrega la página de precios
+        self.page_container.addWidget(HomePage())
+        self.page_container.addWidget(CotizacionesPage())
+        self.page_container.addWidget(PreciosPage())
 
-        # Cambiar al contenido de la página de inicio por defecto
         self.page_container.setCurrentIndex(0)
 
-        # Agregar el contenedor de páginas al diseño del cuerpo
-        body_layout = QtWidgets.QVBoxLayout()  # Diseño vertical para el área de contenido (body)
+        body_layout = QtWidgets.QVBoxLayout()
         body_layout.setContentsMargins(0, 0, 0, 0)
         body_layout.addWidget(self.page_container, stretch=2)
 
-        layout.addLayout(body_layout)  # Agregar el área de contenido a la ventana principal
+        layout.addLayout(body_layout)
+
+        # Establecer el estilo inicial de los botones
+        self.update_redirect_buttons(0)
 
     def show_exit_dialog(self):
         dialog = ExitDialog(self)
-        if dialog.exec_() == QDialog.Accepted:
+        if dialog.exec_() == QtWidgets.QDialog.Accepted:
             self.close()
 
-    # Agregar un método para cambiar la página actual
     def change_page(self, index):
         self.page_container.setCurrentIndex(index)
+        self.update_redirect_buttons(index)
+
+    def update_redirect_buttons(self, current_index):
+        for i, button in enumerate(self.redirect_buttons):
+            if i == current_index:
+                button.setStyleSheet("""
+                    QPushButton {
+                        border-style: outset;
+                        border-radius: 0px;
+                        padding: 6px;
+                        color: hsl(0, 0%, 65%);; 
+                        font: 13pt "Verdana";
+                    }
+                """)
+            else:
+                button.setStyleSheet("""
+                    QPushButton {
+                        border-style: outset;
+                        border-radius: 0px;
+                        padding: 6px;
+                        color: white; 
+                        font: 13pt "Verdana";
+                    }
+                """)
 
 if __name__ == "__main__":
     import sys
